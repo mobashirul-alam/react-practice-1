57ef9584240be57a40699b5642b9ca20823360aa
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-03-19 01:22:21 +0600

    blog-style-added

--- src/App.js
+++ src/App.js
@@ -10,9 +10,18 @@ function App() {
 }
 
 function Article() {
+
+  const blog = {
+    backgroundColor: 'aquamarine',
+    border: '3px solid royalblue',
+    margin: '25px',
+    padding: '15px',
+    textAlign: 'left'
+  }
+
   return (
     <div>
-      <article className="blog">
+      <article style={blog}>
         <h2>React 6 core concepts</h2>
         <div>
           <p>REACT is a library for building user interfaces.It has 6 core concepts that every web developer should know. These are as follows:</p>
